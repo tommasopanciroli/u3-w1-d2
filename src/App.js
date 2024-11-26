156cc7013d76e524786f716f3adab626c4ef5a4e
tommaso.panciroli <tommypanci@gmail.com>
2024-11-26 12:15:38 +0100

    adds third part of the lesson

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css'
 // allego il foglio di bootstrap una volta per tutte
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ResponsiveNavbar from './components/ResponsiveNavbar'
-
+import MainCarouselContent from './components/MainCarouselContent'
+import { Col, Container, Row } from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
       <header>
-        <ResponsiveNavbar subtitle="Pastasciutte sul web" isFluid={true}/>
+        <ResponsiveNavbar subtitle="Pastasciutte sul web" isFluid={true} />
       </header>
+      <main>
+        <Container>
+          <Row>
+            <Col xs={12} md={8} lg={6}>
+              <MainCarouselContent />
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </div>
   )
 }
